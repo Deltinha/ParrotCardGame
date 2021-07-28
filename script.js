11f1fecf84bfb24b8455409684945750ff029300
Moisés Brandão <moisesbrandao1998@gmail.com>
2021-07-28 09:00:14 -0300

    Increased minimal card number from 2 to 4

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let cardNumber = Number(prompt('Quantas cartas?'));
 
 function cardNumberValidation() {
-    while (isNaN(cardNumber) || cardNumber % 2 !== 0 || cardNumber > 14 || cardNumber < 2){
+    while (isNaN(cardNumber) || cardNumber % 2 !== 0 || cardNumber > 14 || cardNumber < 4){
         cardNumber = Number(prompt('Quantas cartas?'));
     }
 }
