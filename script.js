75ccc1aa35ac975556bd4e6ecdf64cb944d076c7
Moisés Brandão <moisesbrandao1998@gmail.com>
2021-07-28 09:41:23 -0300

    Shuffle function added

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
+const cardList = ['bobrossparrot','explodyparrot','fiestaparrot','metalparrot','revertitparrot','tripletsparrot','unicornparrot'];
+
 let cardNumber = Number(prompt('Quantas cartas?'));
+const shuffledDeck = [];
 
 function cardNumberValidation() {
-    while (isNaN(cardNumber) || cardNumber % 2 !== 0 || cardNumber > 14 || cardNumber < 4){
+    while (isNaN(cardNumber) || cardNumber % 2 !== 0 || cardNumber > 14 || cardNumber < 4)
+    {  
         cardNumber = Number(prompt('Quantas cartas?'));
     }
 }
 
+function shuffleCards(){
+    for (let i = 0; i < 2; i++) {
+        for (let j = 0; j < cardNumber/2; j++) {
+            shuffledDeck.push(cardList[j]);
+        }    
+    }
+    shuffledDeck.sort(compareFunction);
+}
+
+function compareFunction() { 
+	return Math.random() - 0.5; 
+}
+
+
+
+
+
+
+
+
 cardNumberValidation();
+shuffleCards();
+
