fdf6313c331cdce3c56dd20a09198bc97be4ea72
Moisés Brandão <moisesbrandao1998@gmail.com>
2021-08-01 12:29:55 -0300

    Added restart game option at the game end

--- script.js
+++ script.js
@@ -1,24 +1,42 @@
-const cardList = ['bobrossparrot','explodyparrot','fiestaparrot','metalparrot','revertitparrot','tripletsparrot','unicornparrot'];
-
-let cardNumber = Number(prompt('Escolha um número par de cartas entre 4 e 14'));
+const cardPool = ['bobrossparrot','explodyparrot','fiestaparrot','metalparrot','revertitparrot','tripletsparrot','unicornparrot'];
 const shuffledDeck = [];
+let firstCard;
+let secondCard;
+let cardsFlipped = 0;
+let pairsFound = 0;
+let cardQuantity;
+let cardsTable = document.querySelector('ul');
+let gameBusy = false;
 
-function cardNumberValidation() {
-    while (!cardNumber || cardNumber % 2 !== 0 || cardNumber > 14 || cardNumber < 4)
+newGame();
+
+function newGame(){
+
+        cardQuantityValidation();
+        shuffleCards();
+        distributeCards();
+}
+
+
+
+
+function cardQuantityValidation() {
+    while (!cardQuantity || cardQuantity % 2 !== 0 || cardQuantity > 14 || cardQuantity < 4)
     {  
-        cardNumber = Number(prompt('Escolha um número par de cartas entre 4 e 14'));
+        cardQuantity = Number(prompt('Escolha um número par de cartas entre 4 e 14'));
     }
 }
 
 function shuffleCards(){
 
-    cardList.sort(compareFunction);
+    cardPool.sort(compareFunction);
 
     for (let i = 0; i < 2; i++) {
-        for (let j = 0; j < cardNumber/2; j++) {
-            shuffledDeck.push(cardList[j]);
+        for (let j = 0; j < cardQuantity/2; j++) {
+            shuffledDeck.push(cardPool[j]);             //Push cards from cardPool into shuffledDeck twice
         }    
     }
+
     shuffledDeck.sort(compareFunction);
 }
 
@@ -27,25 +45,21 @@ function compareFunction() {
 }
 
 function distributeCards(){
-    for (let i = 0; i < cardNumber; i++) {
-        document.querySelector('.card-pool').innerHTML += `<li class="card" onclick="pickCard(this);">
+    for (let i = 0; i < cardQuantity; i++) {
+        cardsTable.innerHTML += `<li class="card" onclick="pickCard(this);">
         <div class="front-face face">
-            <img src="assets/front.png" alt="" />
+            <img src="assets/front.png" alt="" />${shuffledDeck[i]}
         </div>
         <div class="back-face face">
             <img src="assets/${shuffledDeck[i]}.gif" alt="" />
         </div>
-    </li>`;    
+    </li>`;
     }
 }
 
-    let firstCard;
-    let secondCard;
-    let flipsNumber = 0;
-    let pairsFound = 0;
 function pickCard(pickedCard){
-    if (gameBusy === false){    
-        flipsNumber++;
+    if (gameBusy === false){      //Stops the player from picking new cards if the game is busy flipping cards down
+        cardsFlipped++;
         if (firstCard){
             secondCard = pickedCard;
             gameBusy = true;
@@ -54,9 +68,8 @@ function pickCard(pickedCard){
             firstCard = pickedCard;
         }
 
-        pickedCard.querySelector('.front-face').setAttribute('style','transform: rotateY(-180deg);');
+        pickedCard.querySelector('.front-face').setAttribute('style','transform: rotateY(-180deg);'); //Flips the picked card front face up
         pickedCard.querySelector('.back-face').setAttribute('style','transform: rotateY(0deg);');
-
 
         if (firstCard && secondCard){
                 verifyPair();
@@ -64,15 +77,15 @@ function pickCard(pickedCard){
     }
 
 }
-let gameBusy = false;
+
 function verifyPair(){
     if (firstCard.querySelector('.back-face img').getAttribute('src')===secondCard.querySelector('.back-face img').getAttribute('src')){
         pairsFound++;
         firstCard = undefined;
         secondCard = undefined;
         gameBusy = false;
-        if (pairsFound===cardNumber/2){
-            alert(`Você ganhou em ${flipsNumber} jogadas!`);
+        if (pairsFound===cardQuantity/2){
+            endGame();
         }
         
     }
@@ -84,7 +97,7 @@ function verifyPair(){
 
 function flipCardDown(){
     
-    firstCard.querySelector('.front-face').setAttribute('style','transform: rotateY(0deg);');
+    firstCard.querySelector('.front-face').setAttribute('style','transform: rotateY(0deg);');  //Flips the two picked cards front side down
     firstCard.querySelector('.back-face').setAttribute('style','transform: rotateY(180deg);');
     secondCard.querySelector('.front-face').setAttribute('style','transform: rotateY(0deg);');
     secondCard.querySelector('.back-face').setAttribute('style','transform: rotateY(180deg);');
@@ -93,10 +106,16 @@ function flipCardDown(){
     gameBusy = false;
 }
 
+function endGame(){
+    let playAgain = prompt(`Você ganhou em ${cardsFlipped} jogadas! Deseja jogar novamente?`);
+    if (playAgain==='sim'){
+        cardQuantity = 0;
+        shuffledDeck.length = 0;
+        cardsFlipped = 0;
+        pairsFound = 0;
+        cardsTable.innerHTML = '';
+        newGame();
+    }
+}
 
-
-
-cardNumberValidation();
-shuffleCards();
-distributeCards();
 
